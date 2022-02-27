e5a00a4181ba3ec0c61ca53ba95c83e33e8bc562
Alex <98185072+Alex13070@users.noreply.github.com>
2022-02-27 16:40:38 +0100

    funcionando

--- app/src/main/java/com/proyecto/proyectoUnivdule/InicioSesionActivity.kt
+++ app/src/main/java/com/proyecto/proyectoUnivdule/InicioSesionActivity.kt
@@ -2,10 +2,40 @@ package com.proyecto.proyectoUnivdule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.proyecto.proyectoUnivdule.administracionBBDD.UnivduleDB
 
-class InicioSesionActivity : AppCompatActivity() {
+class InicioSesionActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var bbdd: RoomDatabase
+    private lateinit var etNombreUsuario: EditText
+    private lateinit var etPassword: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_sesion)
+
+        bbdd = Room.databaseBuilder(this, UnivduleDB::class.java, "univdule").allowMainThreadQueries().fallbackToDestructiveMigration().build()
+
+        etNombreUsuario = findViewById(R.id.etInicioSesionNombreUsuario)
+        etPassword = findViewById(R.id.etInicioSesionPassword)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnInicioSesion -> iniciarSesion()
+            R.id.btnRegistro -> registro()
+        }
+    }
+
+    private fun registro() {
+        TODO("Not yet implemented")
+    }
+
+    private fun iniciarSesion() {
+        TODO("Not yet implemented")
     }
 }
