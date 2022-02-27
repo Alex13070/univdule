ec622fff464e352e1a48786a758879986b119381
Alex <98185072+Alex13070@users.noreply.github.com>
2022-02-27 17:46:42 +0100

    Inicio sesion

--- app/src/main/java/com/proyecto/proyectoUnivdule/InicioSesionActivity.kt
+++ app/src/main/java/com/proyecto/proyectoUnivdule/InicioSesionActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.room.Room
@@ -18,6 +20,9 @@ class InicioSesionActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var etNombreUsuario: EditText
     private lateinit var etPassword: EditText
 
+    private lateinit var btnIniciarSesion: Button
+    private lateinit var btnRegistrar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_sesion)
@@ -25,13 +30,21 @@ class InicioSesionActivity : AppCompatActivity(), View.OnClickListener {
         bbdd = Room.databaseBuilder(this, UnivduleDB::class.java, "univdule").allowMainThreadQueries().fallbackToDestructiveMigration().build()
 
         etNombreUsuario = findViewById(R.id.etInicioSesionNombreUsuario)
+
         etPassword = findViewById(R.id.etInicioSesionPassword)
+
+        btnIniciarSesion = findViewById(R.id.btnInicioSesionInicioSesion)
+        btnIniciarSesion.setOnClickListener(this)
+
+        btnRegistrar = findViewById(R.id.btnInicioSesionRegistro)
+        btnRegistrar.setOnClickListener(this)
+
     }
 
-    override fun onClick(v: View) {
-        when (v.id) {
-            R.id.btnInicioSesion -> iniciarSesion()
-            R.id.btnRegistro -> registro()
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnInicioSesionInicioSesion -> comprobarDatos()
+            R.id.btnInicioSesionRegistro -> registro()
         }
     }
 
@@ -40,18 +53,24 @@ class InicioSesionActivity : AppCompatActivity(), View.OnClickListener {
         startActivity(intent)
     }
 
-    private fun iniciarSesion() {
+    private fun comprobarDatos() {
         var nombreUsuario = etNombreUsuario.text.toString()
         var password = etPassword.text.toString()
 
-        var usu: Optional<Usuario> = Optional.of(bbdd.usuarioDAO().findByUserName(nombreUsuario))
+        var usu: Optional<Usuario> = Optional.ofNullable(bbdd.usuarioDAO().findByUserName(nombreUsuario))
 
-        if (usu.isPresent) {
-            var usuario = usu.get()
-
-        }
+        if (usu.isPresent)
+            if (usu.get().clave.equals(password))
+                iniciarSesion()
+            else
+                Toast.makeText(this, "Usuario o contraseña incorrectos.", Toast.LENGTH_SHORT).show()
         else
-            Toast.makeText(this, "El usuario introducido no existe", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "El usuario introducido no existe.", Toast.LENGTH_SHORT).show()
 
+    }
+
+    private fun iniciarSesion() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
